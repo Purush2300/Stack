c645a68e4500d4448aa6367755c1decead8a77ad
purush2300 <reddypurushothm@gmail.com>
2026-07-17 07:13:21 +0530

    Initial commit

---
+++ july17.java
@@ -0,0 +1,14 @@
+public class july17 {
+    public static void main(String[] args) {
+        // String s="  sky in  blue";
+        // String[]s1=s.split("\\s+");
+        // String ans="";
+        // for(int i=s1.length-1;i>=0;i--){
+        //     ans+=s1[i];
+        //     if(i!=0){
+        //         ans+=" ";
+        //     }
+        // }
+        // System.out.println(ans);
+    }
+}
