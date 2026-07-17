acdac4b93db1866bfd5dd8ee15ba872e64ea3d49
purush2300 <reddypurushothm@gmail.com>
2026-07-17 07:24:42 +0530

    Initial commit

--- july17.java
+++ july17.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class july17 {
     public static void main(String[] args) {
         // String s="  sky in  blue";
@@ -10,5 +12,38 @@ public class july17 {
         //     }
         // }
         // System.out.println(ans);
+        HashMap<Character,Character> map1=new HashMap<>();
+          HashMap<Character,Character> map2=new HashMap<>();
+        String s1="ab";
+        String s2="aa";
+        for(int i=0;i<s1.length();i++){
+            char c1=s1.charAt(i);
+            char c2=s2.charAt(i);
+            if(map1.containsKey(c1)){
+                if(map1.get(c1)!=c2){
+                    System.out.println(false+"->map1");
+                    return ;
+                }
+            }
+            else{
+                map1.put(c1, c2);
+            }
+
+
+            if(map2.containsKey(c2)){
+                if(map2.get(c2)!=c1){
+                    System.out.println(false+"->map2");
+                    return;
+                }
+            }
+            else{
+                map2.put(c2, c1);
+            }
+
+
+
+
+        }
+        System.out.println(true);
     }
 }
