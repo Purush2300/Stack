de096dc75895d8f58bbc9c64a1fba26ab5ea8087
purush2300 <reddypurushothm@gmail.com>
2026-07-19 21:16:25 +0530

    Initial commit

--- juky20.java
+++ juky20.java
@@ -1,6 +1,44 @@
 public class juky20 {
  public static void main(String[] args) {
-    int amg=153;
-    System.out.println(amg%10);
+//    int[]arr{}
+   
+//    amstrong(amg);
  }   
+
+    private static int count(int amg) {
+      int cnt=0;
+        while (amg>0) { 
+           cnt++;
+           amg=amg/10;
+       }
+       return cnt;
+    }
+
+    private static void amstrong(int amg) {
+                int total=0;
+                 int orginal=amg;
+                int n=count(amg);
+                int k=n;
+            
+            while(n>0){
+
+                    int last=amg%10;
+                
+                    int w=1;
+                
+                    for(int i=1;i<=k;i++){
+                    w*=last;
+                    
+                    }
+                
+                    total+=w;
+                
+                    w=1;
+                    
+                    amg=amg/10;
+                    n--;
+            }
+            System.out.println(orginal==total);
+
+    }
 }
