73f63800b1517730d85e0bb23277812fc0e84d12
purush2300 <reddypurushothm@gmail.com>
2026-07-21 21:59:54 +0530

    Initial commit

--- july2.java
+++ july2.java
@@ -1,24 +1,38 @@
 
 import java.util.ArrayList;
-import java.util.Stack;
 
 public class july2 {
     public static void main(String[] args) {
-        int[]arr={1,3,2,4};
-        Stack<Integer>s=new Stack<>();
+        int[]arr={1,2,3,4,3};
+    //     Stack<Integer>s=new Stack<>();
         ArrayList<Integer>al=new ArrayList<>();
-        for (int i = arr.length-1; i>=0; i--) {
-            while (!s.isEmpty()&&s.peek()<=arr[i]) { 
-                s.pop();
-            }
-            if(s.isEmpty()){
-                al.add(-1);
-            }
-            else{
-                al.add(s.peek());
-            }
-            s.push(arr[i]);
+    //     for (int i = arr.length-1; i>=0; i--) {
+    //         while (!s.isEmpty()&&s.peek()<=arr[i]) { 
+    //             s.pop();
+    //         }
+    //         if(s.isEmpty()){
+    //             al.add(-1);
+    //         }
+    //         else{
+    //             al.add(s.peek());
+    //         }
+    //         s.push(arr[i]);
+    //     }
+    // System.out.println(al);
+
+
+for(int i=0;i<arr.length;i++){
+  int ans=-1;
+    for(int j=1;j<arr.length;j++){
+        int index=(i+j)%arr.length;
+        if(arr[index]>arr[i]){
+           ans=arr[index];
+            break;
         }
-    System.out.println(al);
+    }
+    al.add(ans);
+}
+
+System.out.println(al);
     }
 }
